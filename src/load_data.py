e63dd524166b6b67bb18731687ef847a7a3a154e
joao_francisco <joaofrancisco.monteiro11@gmail.com>
2026-06-06 15:04:14 -0300

    Arquivo de carregamento e conexão de dados concluído.

--- src/load_data.py
+++ src/load_data.py
@@ -9,3 +9,31 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 env_path = Path(__file__).resolve().parent.parent / 'config' / '.env'
+load_dotenv(env_path)
+
+user = os.getenv('user')
+password = os.getenv('password')
+database = os.getenv('database')
+
+host = 'host.docker.internal'
+
+def get_engine():
+    logging.info(f"\n ➔ Conectando em {host}:5432/{database}")
+    return create_engine(
+        f"postgresql+psycopg2://{user}:{quote_plus(password)}@{host}:5432/{database}"
+    )
+
+engine = get_engine()
+
+def load_database(table_name:str, df):
+    df.to_sql(
+        name=table_name,
+        con=engine,
+        if_exists='append',
+        index = False
+    )
+    logging.info(f"\n✔ Dados Carregados com Sucesso!")
+
+    df_check = pd.read_sql('SELECT * FROM {table_name}', con=engine)
+    logging.info(f"\nTotal de registros na tabela: {len(df_check)}\n")
+    return []
