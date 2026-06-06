ee9b95837e4b83882888b13dc957321c24ab91a5
joao_francisco <joaofrancisco.monteiro11@gmail.com>
2026-06-05 23:43:56 -0300

    Funções 'create_dataframe' e 'normalize_weather_column' criadas. São funções de tratamento de dados.

--- src/transform_data.py
+++ src/transform_data.py
@@ -27,3 +27,26 @@ def create_datafrme(path_name:str) -> pd.DataFrame:
     except IOError as e:
         logging.error(f'Erro ao carregar o arquivo JSON: {e}')
         return pd.DataFrame()
+
+def normalize_weather_column(df:pd.DataFrame) -> pd.DataFrame:
+    df_weather = pd.json_normalize(df['weather'].apply(lambda x: x[0])) 
+
+    df_weather = df_weather.rename(columns={
+        'id':'weather_id',
+        'main':'weather_main',
+        'description':'weather_description',
+        'icon':'weather_icon'
+    })
+
+    df = pd.concat(df, df_weather, axis=1)
+
+    logging.info(f"\n✔ Coluna 'weather' normalizada - {len(df.columns)} colunas") 
+
+    return df
+
+
+def drop_columns():
+    return pd.DataFrame
+
+def rename_columns():
+    return pd.DataFrame
